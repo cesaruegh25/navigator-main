e6701b2dc309a85305b97847d455a8a7291e2d27
cesaruegh25 <csanmal2502@g.educaand.es>
2026-05-23 18:00:22 +0200

    animaciones y logica de nivel y menu pausa

---
+++ Assets/02_Scripts/GameController.cs
@@ -0,0 +1,181 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
+
+public class GameController : MonoBehaviour
+{
+
+    public bool endgame;
+    public bool win = false;
+
+    public static GameController instance;
+    public int escena;
+    public bool nivelMedio = false;
+    public bool nivelDificil = false;
+    private int score;
+    public bool menuPausa = false;
+    private LoadSceneMode mode;
+    [SerializeField] private GameObject pauseMenu;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        reiniciarScore();
+        DontDestroyOnLoad(pauseMenu);
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        escena = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log("Escena actual-0:" + escena);
+        nivelMedio = false;
+        nivelDificil = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menuPausa)
+            {
+                reanudarJuego();
+            }
+            else
+            {
+                pausarJuego();
+            }
+        }
+        //Debug.Log("Escena update:" + escena);
+    }
+
+    public void ganarJuego()
+    {
+        Debug.Log("Ganar Juego");
+        Debug.Log("Escena actual-1:" + escena);
+        winGame();
+
+    }
+
+    public void recivirDaño()
+    {
+        lose();
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void aumentarScore(int cantidad)
+    {
+        score += cantidad;
+    }
+
+    public void disminuirScore(int cantidad)
+    {
+        score -= cantidad;
+    }
+
+    public void pausarJuego()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        menuPausa = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void reanudarJuego()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        menuPausa = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void resetToMenu()
+    {
+        reiniciarScore();
+        escena = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene(escena);
+    }
+    
+    public void EndGame()
+    {
+        Debug.Log("puntuación = " + score);
+        SceneManager.LoadScene(escena);
+        endgame = true;
+
+    }
+    private void reiniciarScore()
+    {
+        score = 0;
+    }
+    private void winGame()
+    {
+        //carga pantalla de win
+        Debug.Log("Has ganado");
+        Debug.Log("numero de escena-2:" + escena);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        aumentarScore(100);
+        if (escena == 1)
+        {
+            Debug.Log("Cargando siguiente nivel:" + escena);
+            escena = 0;
+            nivelMedio = true;
+            SceneManager.LoadScene(escena);
+            Debug.Log("Nivel cargado:" + escena);
+            Debug.Log("Score actual:" + score);
+        }
+        if (escena == 2)
+        {
+            Debug.Log("Cargando siguiente nivel:" + escena);
+            escena = 0;
+            nivelDificil = true;
+            SceneManager.LoadScene(escena);
+            Debug.Log("Nivel cargado:" + escena);
+            Debug.Log("Score actual:" + score);
+        }
+        if (escena == 3)
+        {
+            Debug.Log("Juego terminado");
+            escena = 0;
+            win = true;
+            EndGame();
+        }
+    }
+    private void lose()
+    {
+        Debug.Log("Has perdido");
+        win = false;
+        escena = 0;
+        EndGame();
+        Debug.Log("Score actual:" + score);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void salir() { 
+        Application.Quit();
+    }
+    public void backToNivel() {
+        escena = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene(escena);
+    }
+}
---
+++ Assets/02_Scripts/controlBtn.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controlBtn : MonoBehaviour
+{
+
+    public GameObject startMenu;
+    public GameObject nivelesMenu;
+    public GameObject endMenu;
+
+    public GameObject btnNivelFacil;
+    public GameObject btnNivelMedio;
+    public GameObject btnNivelDificil;
+    public GameObject victoria;
+    public GameObject derrota;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startMenu.SetActive(true);
+        nivelesMenu.SetActive(false);
+        endMenu.SetActive(false);
+        btnNivelMedio.SetActive(false);
+        btnNivelDificil.SetActive(false);
+        victoria.SetActive(false);
+        derrota.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameController.instance.endgame)
+        {
+            mostrarEndMenu();
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Debug.Log("Mostrando menu de fin de juego");
+            desactivarBotonesDificultad();
+        }
+        if(GameController.instance.nivelMedio && !btnNivelMedio.activeSelf)
+        {
+            mostrarNivelesMenu();
+            activarBotoneDificultadMedia();
+        }
+        if(GameController.instance.nivelDificil && !btnNivelDificil.activeSelf)
+        {
+            mostrarNivelesMenu();
+            activarBotoneDificultadDificil();
+        }
+    }
+
+    public void mostrarNivelesMenu()
+    {
+        startMenu.SetActive(false);
+        nivelesMenu.SetActive(true);
+    }
+
+    public void ocultarNivelesMenu()
+    {
+        startMenu.SetActive(true);
+        nivelesMenu.SetActive(false);
+    }
+    public void mostrarEndMenu()
+    {
+        //Debug.Log("Dentro de mostrar end menu");
+        endMenu.SetActive(true);
+        startMenu.SetActive(false);
+        nivelesMenu.SetActive(false);
+        if (GameController.instance.win)
+        {
+            victoria.SetActive(true);
+        }
+
+        else
+        {
+            derrota.SetActive(true);
+        }
+        GameController.instance.nivelMedio = false;
+        GameController.instance.nivelDificil = false;
+        GameController.instance.endgame = false;
+
+    }
+    public void ocultarEndMenu()
+    {
+        endMenu.SetActive(false);
+        nivelesMenu.SetActive(false);
+        startMenu.SetActive(true);
+    }
+    public void salirJuego()
+    {
+        Application.Quit();
+    }
+    public void cargarNivelFacil()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
+        GameController.instance.escena = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Debug.Log("Cargando nivel facil, escena:" + GameController.instance.escena);
+    }
+    public void cargarNivelMedio()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(2);
+        GameController.instance.escena = 2;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void cargarNivelDificil()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(3);
+        GameController.instance.escena = 3;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void activarBotoneDificultadMedia()
+    {
+        btnNivelMedio.SetActive(true);
+    }
+    public void activarBotoneDificultadDificil()
+    {
+        btnNivelDificil.SetActive(true);
+    }
+    public void desactivarBotonesDificultad()
+    {
+        btnNivelMedio.SetActive(false);
+        btnNivelDificil.SetActive(false);
+    }
+    
+}
